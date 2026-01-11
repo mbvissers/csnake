06d9d2f0799bb334a15046b57f5c0ad05bdcae7f
mbvissers <mb.vissers@gmail.com>
2026-01-11 18:40:26 +0100

    show arena

--- src/main.cpp
+++ src/main.cpp
@@ -9,10 +9,22 @@ int main() {
   noecho();
   curs_set(0);
 
-  mvprintw(1, 1, "Size: %i x %i", LINES, COLS);
+  int min_x = 2;
+  int max_x = COLS - 3;
+  int min_y = 2;
+  int max_y = LINES - 3;
+
+  mvprintw(1, 2, "Size: %i x %i", max_y, max_x);
+  for (int y = min_y; y <= max_y; y++) {
+    for (int x = min_x; x <= max_x; x++){
+      bool is_wall = (x == min_x || x == max_x) || (y == min_y || y == max_y);
+      if (is_wall)
+        mvprintw(y, x, "#");
+    }
+  }
   refresh();
 
-  std::this_thread::sleep_for(std::chrono::seconds(2));
+  std::this_thread::sleep_for(std::chrono::seconds(5));
 
   endwin();
   return 0;
