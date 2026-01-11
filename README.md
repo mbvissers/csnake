ee7dbecdfa41ae3e000af2d4111705e4826ab567
mbvissers <mb.vissers@gmail.com>
2026-01-11 21:33:00 +0100

    update readme

--- README.md
+++ README.md
@@ -18,8 +18,13 @@ And the following to build to program.
 g++ src/main.cpp -std=c++20 -lncurses -o csnake
 ```
 
-And then of course, to following to run the file.
+And then of course, the following to run the file.
 
 ```
 ./csnake
 ```
+
+## Note to self
+
+To search for features in ncurses. Check the man page with `man ncurses`, and if you find a function,
+go deeper into its docs using `man 3 <function>`
--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,8 @@
 #include <chrono>
-#include <ncurses.h>
+#include <ncursesw/ncurses.h>
 #include <thread>
+
+void draw_arena(int, int, int, int);
 
 int main() {
   WINDOW *my_win;
@@ -15,17 +17,21 @@ int main() {
   int max_y = LINES - 3;
 
   mvprintw(1, 2, "Size: %i x %i", max_y, max_x);
-  for (int y = min_y; y <= max_y; y++) {
-    for (int x = min_x; x <= max_x; x++){
-      bool is_wall = (x == min_x || x == max_x) || (y == min_y || y == max_y);
-      if (is_wall)
-        mvprintw(y, x, "#");
-    }
-  }
+  draw_arena(min_x, max_x, min_y, max_y);
   refresh();
 
   std::this_thread::sleep_for(std::chrono::seconds(5));
 
   endwin();
   return 0;
+}
+
+void draw_arena(int min_x, int max_x, int min_y, int max_y) {
+  for (int y = min_y; y <= max_y; y++) {
+    for (int x = min_x; x <= max_x; x++) {
+      bool is_wall = (x == min_x || x == max_x) || (y == min_y || y == max_y);
+      if (is_wall)
+        mvprintw(y, x, "#");
+    }
+  }
 }
